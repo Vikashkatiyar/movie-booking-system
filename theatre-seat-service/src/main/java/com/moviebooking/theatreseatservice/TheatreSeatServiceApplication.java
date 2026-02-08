5d403b507338397c93eff81187bc9908558e2b20
Vikash Katiyar <rishabktr018@gmail.com>
2026-02-09 00:28:18 +0530

    initialize core microservices for movie booking system

---
+++ theatre-seat-service/src/main/java/com/moviebooking/theatreseatservice/TheatreSeatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moviebooking.theatreseatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreSeatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatreSeatServiceApplication.class, args);
+	}
+
+}
---
+++ theatre-seat-service/src/test/java/com/moviebooking/theatreseatservice/TheatreSeatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebooking.theatreseatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreSeatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
