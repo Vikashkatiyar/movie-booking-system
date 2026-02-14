982709f5ddf9fcb65b192d27cf6ba742513326f5
Vikash Katiyar <rishabktr018@gmail.com>
2026-02-14 16:01:07 +0530

    feat(auth-phase1): implement basic JWT authentication with register & login APIs

---
+++ user-service/src/main/java/com/moviebooking/userservice/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.moviebooking.userservice.repo;
+
+
+import com.moviebooking.userservice.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
